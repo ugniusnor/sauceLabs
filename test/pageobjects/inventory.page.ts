09835d08eaf02adac3e9a6449c8cbe90750f9279
Ugnius Norkevicius <ugnn97@gmail.com>
2021-08-03 21:32:09 +0300

    optimized selectors for product

--- test/pageobjects/inventory.page.ts
+++ test/pageobjects/inventory.page.ts
@@ -7,23 +7,35 @@ class InventoryPage extends Page {
   /**
    * define selectors using getter methods
    */
-   get wrongCredentialsErrorText():WebdriverIO.Element  {
+  get wrongCredentialsErrorText(): WebdriverIO.Element {
     return $('h3[data-test="error"]');
-  }get catalogItemTitle():WebdriverIO.Element {return $(`a`)}
-  public  expectedNotLogedErrorText:string ="Epic sadface: You can only access '/inventory.html' when you are logged in."
-
-  async login(username: string, password: string) {
-   
   }
-  async notLogedErrorText():Promise<string> {
+  get catalogItemTitle(): WebdriverIO.Element {
+    return $(`a`);
+  }
+  get productBlock():WebdriverIO.Element {return $( 'div .inventory_item_desc')}
+
+
+  public expectedNotLogedErrorText: string =
+    "Epic sadface: You can only access '/inventory.html' when you are logged in.";
+
+  async login(username: string, password: string) {}
+  async notLogedErrorText(): Promise<string> {
     const text = await (await this.wrongCredentialsErrorText).getText();
     return await text;
   }
-async getCatalogItemTitle(id:number):Promise<string>{
-    const itemName:WebdriverIO.Element=  await $(`a#item_${id}_title_link div`)
-    return itemName.getText();
+
+  async getProductFromCatalog(id:number) {
+    const uniqueId = $( `a#item_${id}_title_link div`);
+    const parent = (await uniqueId.parentElement()).parentElement();
+    let product:Product = {
+        name:await (await parent).$(`div.inventory_item_label div.inventory_item_name`).getText(),
+        description:(await parent).$('div.inventory_item_label div.inventory_item_desc').getText()
+    };
+    return product;
     
-}   
+  }
+
   open() {
     return super.open("inventory.html");
   }
--- test/specs/Inventory.spec.ts
+++ test/specs/Inventory.spec.ts
@@ -1,23 +1,24 @@
 import { config } from "../../wdio.conf";
 import InventoryPage from "../pageobjects/inventory.page";
 import LoginWithCookies from "../support/SetLoginCookies";
-describe.only("", () => {
-  it('User is not able to access catalog page before login', async ()=>{
+import productData from "../testData/catalaog/productsData";
+describe("", () => {
+  it("User is not able to access catalog page before login", async () => {
     await InventoryPage.open();
     // await expect(await InventoryPage.notLogedErrorText()).toBe(InventoryPage.expectedNotLogedErrorText)
     await expect(await browser.getUrl()).toBe(config.baseUrl);
-    
-  })
-  before( ()=>{
-    //setting cookies for next text 
-     InventoryPage.open();
-     LoginWithCookies("session-username", "standard_user");
-  })
-
-
-  it("Catalog should have correct products listed", async () => {
-       await InventoryPage.open();
-      await expect(await InventoryPage.getCatalogItemTitle(0)).toBe("dasd")
   });
- 
+  before(() => {
+    //setting cookies for next text
+    InventoryPage.open();
+    LoginWithCookies("session-username", "standard_user");
+  });
+
+  productData.forEach(({ id, name, description }) => {
+    it("Catalog should have correct products listed", async () => {
+      await InventoryPage.open();
+      const product = await InventoryPage.getProductFromCatalog(id);
+      await expect(await product.name).toBe(name);
+    });
+  });
 });
